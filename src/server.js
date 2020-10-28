e3e91fbc5b94c75acb1f163e9c1ac9097653e0e1
Benjamin Kim <benkim96@gmail.com>
2020-10-28 16:41:14 -0700

    docker: working node applicatioN

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
+app.get('/', (req, res) => res.json({ message: 'Hello World' }))
+
 app.listen(process.env.API_PORT, function() {
 	console.log('listening on ' + process.env.API_PORT);
 });
