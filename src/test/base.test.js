169f6a57d85f71f2e3345336f1d94c012b87d357
Benjamin Kim <benkim96@gmail.com>
2020-10-30 12:24:34 -0700

    working test checkpoint

--- src/test/base.test.js
+++ src/test/base.test.js
@@ -1,5 +1,18 @@
+const request = require('supertest')
+const app = require('../server')
+import "babel-polyfill"
+
 describe("Base Test", () => {
 	it("should test that tests are working", () => {
 		expect(true).toBe(true)
 	})
 });
+
+describe('Get Endpoints', () => {
+  it('should get 10 random trivia questions', async () => {
+    const res = await request(app)
+      .get('/trivia')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.length).toEqual(10)
+  })
+})
