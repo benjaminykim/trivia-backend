877d4a81724be3f4e9f4d22640ee06721b51a506
Benjamin Kim <benkim96@gmail.com>
2020-10-29 20:39:33 -0700

    integrate sequelize migrations

--- src/server.js
+++ src/server.js
@@ -1,24 +1,7 @@
 const express = require('express');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 const app = express();
-const Sequelize = require('sequelize');
-
-const dbConfig  = require('./config/database').database;
-
-const sequelize = new Sequelize(
-												dbConfig.database,
-												dbConfig.username,
-												dbConfig.password,
-												dbConfig);
-
-sequelize.authenticate()
-	.then(() => {
-		console.log('Connection successfully established.');
-	})
-	.catch(err => {
-		console.error('Unable to connect to db:', err);
-	});
-
+const { db } = require('./models');
 
 app.get('/', (req, res) => res.json({ message: 'Hello World' }))
 
